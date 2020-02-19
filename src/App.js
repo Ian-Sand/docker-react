3ffda04fc251b7e76c205a607bce790486fa9f6f
Ian Sanders <IanSanders.28@gmail.com>
2020-02-19 15:06:43 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          will still change.. But not currently happening ... Blah blah blah 
         </p>
         <a
           className="App-link"
